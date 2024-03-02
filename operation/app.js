c8f076be336adf46af0f36c29a9ff5b406bea907
sadia <161947312+sadiarajpoot@users.noreply.github.com>
2024-03-02 09:28:23 -0800

    Add files via upload

---
+++ operation/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let a = 10;
+let b = 12;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a % b);
+console.log(a ** b);
---
+++ operation/app.ts
@@ -0,0 +1,7 @@
+let a = 10
+let b = 12
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a%b);
+console.log(a**b);
