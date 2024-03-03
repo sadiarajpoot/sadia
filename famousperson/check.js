abb1c69c3ab530f6798cdd053ac2d4b3b49c1b7b
sadia <161947312+sadiarajpoot@users.noreply.github.com>
2024-03-03 02:37:50 -0800

    Add files via upload
    governer siindh kamran khan tassori

---
+++ famousperson/check.js
@@ -0,0 +1,2 @@
+var FamousPerson = ("Eric");
+console.log("Hello", FamousPerson, "would you like to learn some python today?");
---
+++ famousperson/check.ts
@@ -0,0 +1,2 @@
+let FamousPerson = ("Eric");
+console.log("Hello" , FamousPerson , "would you like to learn some python today?");
