5aec0569fe2de6a883f346dd7492a6b51592c31d
sadia <161947312+sadiarajpoot@users.noreply.github.com>
2024-03-09 10:46:57 -0800

    typescript

---
+++ a--anda++/app.js
@@ -0,0 +1,20 @@
+"use strict";
+//let a = 8     //preincrement
+//console.log(a)
+//console.log(++a)
+//console.log(a)
+//let a = 6
+//console.log(a)  //postincrement
+//console.log(a++)
+//console.log(a)
+//console.log(a)
+//let a = 11
+//console.log(a)  //predecrement
+//console.log(--a)
+//console.log(a)
+//console.log(a)
+let a = 20;
+console.log(a); //postdecrement
+console.log(a--);
+console.log(a);
+console.log(a);
---
+++ a--anda++/app.ts
@@ -0,0 +1,22 @@
+//let a = 8     //preincrement
+//console.log(a)
+//console.log(++a)   //result 9
+//console.log(a)     //result 9
+
+//let a = 6
+//console.log(a)  //postincrement
+//console.log(a++)  //result 6
+//console.log(a)   //result 7
+//console.log(a)
+
+//let a = 11
+//console.log(a)  //predecrement
+//console.log(--a)   //result 10
+//console.log(a)     //result 10
+//console.log(a)
+
+let a = 20
+console.log(a)  //postdecrement
+console.log(a--)  //result 20
+console.log(a)   //result 19
+console.log(a)
